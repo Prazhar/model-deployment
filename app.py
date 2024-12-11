c837f986ca20a6fee9f883cd711e12e7ed9f580f
Prazhar <135412419+Prazhar@users.noreply.github.com>
2024-12-11 06:34:48 +0000

    Another try for numpy

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, request
 import pickle
 
+app = Flask(__name__)
 cv = pickle.load(open("models/cv.pkl", "rb"))
 clf = pickle.load(open("models/clf.pkl", "rb"))
 
-app = Flask(__name__)
+
 
 @app.route("/")
 def home():
@@ -16,11 +17,11 @@ def home():
 @app.route("/predict", methods=["POST"])
 def predict():
     # if request.method == "POST":
-    email = request.form.get("content")
+    email = request.form.get('content')
     tokenized_email = cv.transform([email])
-    predictions = clf.predict(tokenized_email)
-    predictions = 1 if predictions == 1 else -1
-    return render_template("index.html", predictions = predictions, email = email)
+    prediction = clf.predict(tokenized_email)
+    prediction = 1 if prediction == 1 else -1
+    return render_template("index.html", prediction = prediction, email = email)
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
